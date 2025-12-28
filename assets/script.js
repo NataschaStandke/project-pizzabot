258f9ee654fa2a59b1a3c0fb06807eb8b51d8d53
natascha <natascha@i-comp.de>
2025-12-28 12:53:59 +0100

    create pizza game

---
+++ assets/script.js
@@ -0,0 +1,15 @@
+const slider = document.getElementById("styleSlider")
+const pizzaBase = document.getElementById("pizzaBase")
+
+slider.addEventListener("input", () => {
+  pizzaBase.src = `assets/pizzabase-${slider.value}.png`;
+});
+
+const checkboxes = document.querySelectorAll("input[type='checkbox]");
+
+checkboxes.forEach(box => {
+  box.addEventListener("change", () => {
+    const topping = document.getElementById(box.dataset.top);
+    topping.classList.toggle("hidden", !box.checked);
+  });
+});
